0fea63dfd4732f896ec85b9287749d79da5d6db9
Samuel Parchert <parchertsam@gmail.com>
2019-07-15 17:51:24 +1000

    modified to work with AusmashPy update

--- player_data_fetcher.py
+++ player_data_fetcher.py
@@ -6,8 +6,23 @@ import api_requests as api
 BOT_PREFIX = "?"
 TOKEN = os.environ.get("TOKEN")
 
-game = api.Game(13)
+GAME = api.Game(13)
 
+REGIONS = []
+for region in api.Region.get_regions():
+    REGIONS.append(api.Region(region['ID']))
+
+def search_region(term):
+    for region in REGIONS:
+        if region.name.upper() == term.upper() or region.short == term.upper():
+            return region
+    return None
+
+def find_player(name, region):
+    try:
+        return api.Player(api.Player.get_player(name=name, region=search_region(region))["ID"])
+    except api.IncorrectArgumentsError:
+        return -1
 
 client = Bot(command_prefix=BOT_PREFIX,
              activity=Game(name="type " + BOT_PREFIX + "help for help"))
@@ -23,13 +38,29 @@ async def on_ready():
 
 @client.command()
 async def elo(ctx, player_name, player_region):
-    print(player_name)
-    print(player_region)
-    player = api.Player(api.Player.get_player(name=player_name, region=player_region)["ID"])
+    player = find_player(player_name, player_region)
+    if player == -1:
+        await ctx.send("Please use a valid region.")
+        return
     for result in player.get_elo():
-        if result["Game"]["ID"] == game.id:
+        if result["Game"]["ID"] == GAME.id:
             await ctx.send("{} from {} has {} elo.".format(player_name, player_region, result["Elo"]))
             return
-    await ctx.send("{} from {} has no recorded data for {}".format(player_name, player_region, game.name))
+    await ctx.send("{} from {} has no recorded elo data for {}".format(player_name, player_region, GAME.name))
 
+
+@client.command()
+async def trueskill(ctx, player_name, player_region):
+    player = find_player(player_name, player_region)
+    if player == -1:
+        await ctx.send("Please use a valid region.")
+        return
+    for result in player.get_trueskill():
+        if result["Game"]["ID"] == GAME.id:
+            await ctx.send("{} from {} has a mean trueskill of {}.".format(player_name, player_region.name, result["Mean"]))
+            return
+    await ctx.send("{} from {} has no recorded trueskill rating for {}".format(player_name, player_region.name, GAME.name))
 client.run(TOKEN)
+
+#@client.command()
+#async def
